5ddc31e6f1f858992bb0c9616b6c058dc1b6c232
Fabio Kung <fabio.kung@gmail.com>
2012-10-12 01:58:48 -0700

    renaming

--- sync.sh
+++ sync.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 apt-ftparchive generate apt-ftparchive.conf
-apt-ftparchive -c lucid.conf release www/dists/lucid > www/dists/lucid/Release
-apt-ftparchive -c precise.conf release www/dists/precise > www/dists/precise/Release
+apt-ftparchive -c lucid.conf release ubuntu/dists/lucid > ubuntu/dists/lucid/Release
+apt-ftparchive -c precise.conf release ubuntu/dists/precise > ubuntu/dists/precise/Release
