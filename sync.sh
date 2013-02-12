2d3b6742bce78144035dab051c7ae5987fda9182
Nando Vieira <fnando.vieira@gmail.com>
2013-02-12 03:42:13 -0200

    Added command for signing repo with GPG.

--- sync.sh
+++ sync.sh
@@ -2,3 +2,7 @@
 apt-ftparchive generate apt-ftparchive.conf
 apt-ftparchive -c lucid.conf release ubuntu/dists/lucid > ubuntu/dists/lucid/Release
 apt-ftparchive -c precise.conf release ubuntu/dists/precise > ubuntu/dists/precise/Release
+
+# Uncomment the following lines if you want to sign this repo.
+# gpg --output ubuntu/dists/lucid/Release.gpg -ba ubuntu/dists/lucid/Release
+# gpg --output ubuntu/dists/precise/Release.gpg -ba ubuntu/dists/precise/Release
